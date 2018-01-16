e5693c24811ae7ae2e782d285641ced0d2185ad5
sandeepkkv <kkvsandeep92@gmail.com>
2018-01-15 20:49:02 -0800

    File added

---
+++ Hello.java
@@ -0,0 +1,16 @@
+ Volume in drive C is Windows
+ Volume Serial Number is B4A5-AF31
+
+ Directory of C:\Users\sandy\Desktop\Jenkins
+
+01/15/2018  08:47 PM    <DIR>          .
+01/15/2018  08:47 PM    <DIR>          ..
+01/14/2018  08:33 PM               152 Hell.java.txt
+01/15/2018  08:47 PM                 0 Hello.java
+01/14/2018  08:31 PM               152 Hello.java.rtf
+01/14/2018  05:02 PM    <DIR>          Jenkins-cli
+01/14/2018  04:28 PM       122,335,232 jenkins.msi
+01/14/2018  04:37 PM        74,294,776 jenkins.war
+01/14/2018  05:20 PM           315,713 websphere-deployer-v1.4.7.hpi
+               6 File(s)    196,946,025 bytes
+               3 Dir(s)  274,557,652,992 bytes free
